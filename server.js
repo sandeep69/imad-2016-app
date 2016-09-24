6dda0215962a1880c35ab4f5f9e90893ac0deb87
sandeep69 <s_j_abraham@hotmail.com>
2016-09-24 10:55:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article = {
+    title:"Article One - Susan",
+    heading:"Article One",
+    date:"24 September 2016",
+    content:`<p>This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool</p>
+                <p>This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool</p>
+                <p>This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool</p>`
+    
+};
+var container = {
+    
+}
+
+function createTemplate(data){
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+
+var htmlTemplate= `<html>
+
+    <head>
+        <title> ${title}</title>
+        <link rel="stylesheet" href="/ui/style.css/">
+    </head>
+    <body>
+        
+        <div class="container">
+            <div>
+                <a href="/"> Home</a>
+                <hr/>
+            </div>
+            <div>
+                <h2> ${heading}</h2>
+            </div>
+            <div>
+                <h3>Date: ${date}</h3>
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +66,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/profile', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+  res.send(createTemplate(article));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
