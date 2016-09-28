142b6e2bffc3fc17fcac3db39c678aa16824e481
sandeep69 <s_j_abraham@hotmail.com>
2016-09-28 21:13:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/:articleName', function (req, res) {
 });
 
 
-app.get('/aboutMe.html', function (req, res) {
+app.get('/aboutMe', function (req, res) {
     console.log("I am here");
   res.sendFile(path.join(__dirname, 'ui', 'aboutMe.html'));
 });
