88a2ee593a31363d2675a29d8b6a4158136a570f
sandeep69 <s_j_abraham@hotmail.com>
2016-09-29 20:04:02 +0530

    [imad-console] Updates ui/app.js

--- ui/app.js
+++ ui/app.js
@@ -5,9 +5,7 @@ var main = function() {
         $('.menu').animate({
             left:'0px'
         },200);
-        $('.aboutBody').animate({
-            left:'285px'
-        },200);
+      
     });
 
 };
