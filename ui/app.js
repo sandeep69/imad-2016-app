e1dabc66595ae054c6f00181b4252fb967953eaf
sandeep69 <s_j_abraham@hotmail.com>
2016-09-29 10:46:47 +0530

    [imad-console] Updates ui/app.js

--- ui/app.js
+++ ui/app.js
@@ -1,10 +1,10 @@
 var main = function() {
-    $('.menuHeading').click(function() = {
+    $('.menuHeading').click(function() {
         $('.menu').animate({
             left:'0px'
         },200);
     });
 
-}
+};
 
 $(document).ready(main);
