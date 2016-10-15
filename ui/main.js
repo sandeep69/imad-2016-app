32385ff53ee098325b5122a5e5fb08de66761d62
sandeep69 <s_j_abraham@hotmail.com>
2016-10-16 00:17:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,16 +4,16 @@ var button= document.getElementById("counter");
 button.onclick = function(){
     //get the response
     var request = new XMLHttpRequest();
-    alert("got reques");
+   
     
     //check if response status has changed
     request.onreadystatechange = function(){
-          alert("got response successfully");
+         
         //check it result has been loaded
         if(request.readyState === XMLHttpRequest.DONE){
             //check if it was a success
             if (request.status === 200) {
-                 alert("got response successfully");
+                
                 var counter = request.responseText;
                 var span= document.getElementById("count");
                 span.innerHTML = counter.toString(); 
@@ -22,7 +22,7 @@ button.onclick = function(){
         //make a request to get the counter 
         
     };
-    alert ("request to be made");
+  
     request.open('GET', 'http://sandeep69.imad.hasura-app.io/counter',true);
     request.send(null);
     
