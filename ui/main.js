02adb86fe871f468fbc45208c3635c27f943c8e5
sandeep69 <s_j_abraham@hotmail.com>
2016-10-15 23:30:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 
+var button= document.getElementById("counter");
+var counter=0;
+button.onclick = function(){
+    counter++;
+    var span= document.getElementById("count");
+    span.innerHTML = counter.toString();
+}
