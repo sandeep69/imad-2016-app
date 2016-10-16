fb70f0d001ab4daca85a30d3d6fa4a373c2ad7ff
sandeep69 <s_j_abraham@hotmail.com>
2016-10-16 11:08:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -70,6 +70,7 @@ submitB.onclick = function(){
 
 //adding comments in article
 
+window.onload = function() {
 var commentSB= document.getElementById('commentB');
     alert("submit clicked");
 commentSB.onclick = function(){
@@ -107,4 +108,5 @@ commentSB.onclick = function(){
     request.open('GET', 'http://sandeep69.imad.hasura-app.io/article-one/comment_list?comment='+user.value,true);
     request.send(null);
  
+};
 };
