d04626efe70180990ce23024c6ad9fba87cc48a4
sandeep69 <s_j_abraham@hotmail.com>
2016-10-16 13:36:14 +0530

    [imad-console] Updates ui/app.js

--- ui/app.js
+++ ui/app.js
@@ -35,14 +35,13 @@ commentSB.onclick = function(){
     
     };
      var user = document.getElementById("comment");
-     var pageH = document.getElementById("check");
+     var pageH = document.getElementById("myHeader");
      pageH.innerHTML = 'NEW';
-     alert(pageH.value);
      var page;
      if (pageH.value === 'article one') page='article-one';
      else if(pageH.value === 'article two') page='article-two';
      else if(pageH.value === 'article three') page='article-three';
-    alert(page);
+
     request.open('GET', 'http://sandeep69.imad.hasura-app.io/'+page+'/comment_list?comment='+user.value,true);
     request.send(null);
  
