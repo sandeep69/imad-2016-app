d0d4ee718239599a937f7c0c43c36cdf3e5cc684
sandeep69 <s_j_abraham@hotmail.com>
2016-10-16 12:08:06 +0530

    [imad-console] Updates ui/app.js

--- ui/app.js
+++ ui/app.js
@@ -40,7 +40,7 @@ commentSB.onclick = function(){
      if (pageH.value == 'article one') page='article-one';
      else if(pageH.value == 'article two') page='article-two';
      else if(pageH.value == 'article three') page='article-three';
-
+    alert(page);
     request.open('GET', 'http://sandeep69.imad.hasura-app.io/'+page+'/comment_list?comment='+user.value,true);
     request.send(null);
  
