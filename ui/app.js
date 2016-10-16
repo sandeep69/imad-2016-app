64b74ba7167d3c764be86100d9bdb6a01029358c
sandeep69 <s_j_abraham@hotmail.com>
2016-10-16 12:17:34 +0530

    [imad-console] Updates ui/app.js

--- ui/app.js
+++ ui/app.js
@@ -35,7 +35,7 @@ commentSB.onclick = function(){
     
     };
      var user = document.getElementById("comment");
-     var pageH=document.getElementById("pageHeading");
+     var pageH=document.getElementById("PH");
         alert(pageH.value);
      var page;
      if (pageH.value === 'article one') page='article-one';
