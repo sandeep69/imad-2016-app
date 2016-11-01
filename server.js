fd1042e106c6e5510a25fd6d20364ed1ed5f57cf
sandeep69 <s_j_abraham@hotmail.com>
2016-11-01 12:56:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,26 +139,6 @@ app.get('/:an/comment_list', function (req, res) {
      
 });
 
-app.get('/article/:articleName', function (req, res) {
-
-  var articleName = req.params.articleName;    
-
-  pool.query("SELECT * FROM article WHERE title='" + articleName +"'", function(err,result){
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          if (result.rows.length === 0){
-              res.status(404).send("Article not found");
-          }
-          else {
-             var articleData = result.rows[0];
-             res.send(createTemplate(articleData));
-          }
-      }
-  });
-  
-});
-
 
 
 
