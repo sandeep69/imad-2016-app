33e22f288ba97d14ba68a797cd1a0e002fa4dd91
sandeep69 <s_j_abraham@hotmail.com>
2016-11-02 10:37:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,31 +15,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var article = {
-    'article-one': {
-                    title:"Article One - Susan",
-                    heading:"Article one",
-                    date:"24 September 2016",
-                    content:`<p>This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool</p>
-                                <p>This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool</p>
-                                <p>This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool</p>`
-                    
-                },
-    'article-two': {
-                    title:"Article two - Susan",
-                    heading:"Article-two",
-                    date:"24 September 2016",
-                    content:`<p>This is the second article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool</p>
-                                <p>This is the second article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool</p>
-                                <p>This is the second article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool. This is the second article. I am so excited. This is pretty cool</p>`
-                },
-    'article-three': {
-            title:"Article three - Susan",
-                    heading:"Article three",
-                    date:"24 September 2016",
-                    content:`<p>This is the third article. I am so excited. This is pretty cool. This is the third article. I am so excited. This is pretty cool. This is the third article. I am so excited. This is pretty cool. This is the first article. I am so excited. This is pretty cool. This is the third article. I am so excited. This is pretty cool. This is the third article. I am so excited. This is pretty cool</p>`
-                },
-    };
 
 function createTemplate(data){
 var title = data.title;
