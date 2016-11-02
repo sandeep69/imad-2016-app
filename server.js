197e3f19a214c00ac1b80bfe13ebb721931e020b
sandeep69 <s_j_abraham@hotmail.com>
2016-11-02 11:05:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/article/:articleName', function (req, res) {
 
   var articleName = req.params.articleName;    
 
-  pool.query("SELECT * FROM article WHERE title = $1" ,[articleName], function(err,result){
+  pool.query("SELECT * FROM article WHERE title='" + articleName +"'", function(err,result){
       if (err) {
           res.status(500).send(err.toString());
       } else {
