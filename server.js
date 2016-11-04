a341fd6f96a385bba75d91d4e3ecc0d496f54888
sandeep69 <s_j_abraham@hotmail.com>
2016-11-04 08:26:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,9 +136,9 @@ app.get('/:an/comment_list', function (req, res) {
     var feedback = req.query.comment;
     var date = new Date();
     
-    an = "'"+an+"'";
-    feedback = "'"+feedback+"'";
-    date= "'"+date.toDateString()+"'";
+    //an = "'"+an+"'";
+    //feedback = "'"+feedback+"'";
+    //date= "'"+date.toDateString()+"'";
 
     
     console.log(an);
