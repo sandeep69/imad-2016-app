0fcf3d2bcb7705a1db9385d3547e1f7879baec59
sandeep69 <s_j_abraham@hotmail.com>
2016-11-09 18:22:36 +0530

    [imad-console] Updates ui/app.js

--- ui/app.js
+++ ui/app.js
@@ -2,13 +2,11 @@
 
 
 var commentSB= document.getElementById('commentB');
-    alert("submit clicked");
 commentSB.onclick = function(){
     //get the response
     var request = new XMLHttpRequest();
-   
-    alert("submit clicked");
-    
+
+
     //check if response status has changed
     request.onreadystatechange = function(){
          
@@ -26,7 +24,6 @@ commentSB.onclick = function(){
                 console.log(list);
                 var nameList = document.getElementById("commentList");
                 nameList.innerHTML = list;
-                console.log ("inserted comment");
             }
         }
         //make a request to get the counter 
@@ -43,25 +40,6 @@ commentSB.onclick = function(){
     page=pageH.innerHTML; 
     alert("The inner html = "+page);
     
-    //str1.localeCompare(str2);
-    
-    
-   /* for(var i=0;i<page.length;i++){
-        if (page[i]==='w') {
-         alert("article2 detected");
-         page='article-two';
-        }
-        else if(page[i] === 'n') {
-         alert("article1 detected");
-         page='article-one';
-     }
-     else if(page[i] === 'h') {
-          alert("article3 detected");
-         page='article-three';
-     }
-    }
-    */
-
     request.open('GET', 'http://sandeep69.imad.hasura-app.io/'+page+'/comment_list?comment='+user.value,true);
     request.send(null);
  
