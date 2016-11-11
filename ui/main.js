e43511e00489bafd7ce397a7518f47e11e0394df
sandeep69 <s_j_abraham@hotmail.com>
2016-11-11 15:09:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -72,7 +72,7 @@ submitB.onclick = function(){
      var password = document.getElementById("password").value;
      console.log (username);
      console.log (password);
-    request.open('GET', 'http://sandeep69.imad.hasura-app.io/create-user',true);
+    request.open('POST', 'http://sandeep69.imad.hasura-app.io/create-user',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
  
