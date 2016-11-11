91979b12710d0c220a7f1bde9d7e3afe508b0417
sandeep69 <s_j_abraham@hotmail.com>
2016-11-11 18:56:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,11 +28,6 @@ submitB.onclick = function(){
                 alert(request.responseText);
             }
         }
-        //make a request to get the counter 
-        
- 
-   
-    
     };
     
      var username = document.getElementById("username").value;
@@ -40,6 +35,44 @@ submitB.onclick = function(){
      console.log (username);
      console.log (password);
     request.open('POST', 'http://sandeep69.imad.hasura-app.io/create-user',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+ 
+};
+
+
+// to login
+var submitB= document.getElementById('logsubmitButton');
+
+submitB.onclick = function(){
+    
+    alert ('submit clicked');
+    //get the response
+    var request = new XMLHttpRequest();
+   
+    
+    //check if response status has changed
+    request.onreadystatechange = function(){
+         
+        //check it result has been loaded
+        if(request.readyState === XMLHttpRequest.DONE){
+            //check if it was a success
+            if (request.status === 200) {
+  
+                alert ('Successfully logged in');
+
+            }
+            else { 
+                alert(request.responseText);
+            }
+        }
+    };
+    
+     var username = document.getElementById("logusername").value;
+     var password = document.getElementById("logpassword").value;
+     console.log (logusername);
+     console.log (logpassword);
+    request.open('POST', 'http://sandeep69.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
  
