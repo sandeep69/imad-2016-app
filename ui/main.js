f543725f433218154762d58ee6f2c1ae88944af7
sandeep69 <s_j_abraham@hotmail.com>
2016-11-11 15:13:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,8 @@
 
 
-var button= document.getElementById("counter");
 
-button.onclick = function(){
-    //get the response
-    var request = new XMLHttpRequest();
-   
     
-    //check if response status has changed
-    request.onreadystatechange = function(){
-         
-        //check it result has been loaded
-        if(request.readyState === XMLHttpRequest.DONE){
-            //check if it was a success
-            if (request.status === 200) {
-                
-                var counter = request.responseText;
-                var span= document.getElementById("count");
-                span.innerHTML = counter.toString(); 
-            }
-        }
-        //make a request to get the counter 
-        
-    };
-  
-    request.open('GET', 'http://sandeep69.imad.hasura-app.io/counter',true);
-    request.send(null);
-};    
-    
-//to modify the list
+//to create a login account
     
 var submitB= document.getElementById('submitButton');
 
