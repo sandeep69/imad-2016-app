6756249c02c8d07f99d941db6c8fcc7568dbadb8
sandeep69 <s_j_abraham@hotmail.com>
2016-11-12 17:53:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ app.get('/logout', function(req,res){
 // api to get the list of all articles
 app.get('/listOfArticles',function(req,res){
    
-    pool.query ('SELECT * FROM "user" WHERE username=$1',[susan], function(err,result){
+    pool.query ('SELECT * FROM "user" WHERE username=$1',["susan"], function(err,result){
     /*pool.query('SELECT * FROM "article"', function(err,result){*/
     if (err) {
         res.status(500).send(err.toString());
