525cd04025efee93f3d9182e1f2c73b57f87fca3
sandeep69 <s_j_abraham@hotmail.com>
2016-11-12 18:24:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
                 var list='';
                 for(var i=0; i<articleList.length;i++){
                     var day = articleList[i].date;
-                    alert(day.getDate());
+                    alert(day.getMonth());
                     list= list+ '<li> <p>' +  articleList[i].heading + '</p></li>';
                 }
                 console.log(list);
