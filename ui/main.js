84fff0d3760b3820a677be3de413d35759b35b37
sandeep69 <s_j_abraham@hotmail.com>
2016-11-18 22:29:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
                 var list='';
                 for(var i=0; i<articleList.length;i++){
                     var x =articleList[i].content;
-                    list= list+ '<li class="list"> <p><p>'+ articleList[i].heading + '<span class ="likes">Likes: ' +articleList[i].likes + '</span></p>'+ x.substring(3,120)+ '....&emsp;&emsp;&emsp;<a  class = "listHyp" href="/article/'+articleList[i].heading + '"> Read more....</a></p></li>';
+                    list= list+ '<li class="list"> <p><p><span class="listHeading">'+ articleList[i].heading + '</span><span class ="likes">Likes: ' +articleList[i].likes + '</span></p>'+ x.substring(3,120)+ '....&emsp;&emsp;&emsp;<a  class = "listHyp" href="/article/'+articleList[i].heading + '"> Read more....</a></p></li>';
                     list=list+'<li class="hiddenlist"> </li>';
                 }
                 console.log(list);
