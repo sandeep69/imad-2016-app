74ab1d183e29016cd2e1f9aa4d1e099bddb2aa3b
sandeep69 <s_j_abraham@hotmail.com>
2016-11-21 12:50:54 +0530

    [imad-console] Updates ui/app.js

--- ui/app.js
+++ ui/app.js
@@ -3,8 +3,82 @@
 
 $(document).ready(function() {
     // load existing comments 
-    //check if user has logged in - if yes show the commentDisplay box
+   
     //diplay the comment list from dbase
+    
+      //check if user logged in. If so get the username
+      
+     //check if user has logged in - if yes show the commentDisplay box  
+    var request = new XMLHttpRequest();
+
+      //check if response status has changed
+    request.onreadystatechange = function(){
+         
+        //check it result has been loaded
+        if(request.readyState === XMLHttpRequest.DONE){
+            //check if it was a success
+            if (request.status === 200) {
+                var name = request.responseText;
+                console.log(name);
+                
+                // user logged in display enter comment box
+                var commentDisplay = document.getElementById("commentDisplay");
+	             if (commentDisplay !== null){
+	                comment.style.display ="block";
+	             } 
+                
+                //as user is logged in get the list of comments by making next get request
+                var requestList = new XMLHttpRequest();
+                //get the response
+             
+                //check if response status has changed
+                requestList.onreadystatechange = function(){
+         
+                //check it result has been loaded
+                    if(requestList.readyState === XMLHttpRequest.DONE){
+                        //check if it was a success
+                        if (requestList.status === 200) {
+                    
+                            var comments = requestList.responseText;
+                            console.log(comments);
+                            comments = JSON.parse(comments);
+                   
+                            var list='';
+                            for(var i=0; i<comments.length;i++){
+                                console.log (comments[i].user-name);
+                                list= list+ '<li class= "indComment"> <p><span class ="bigBold">' + comments[i].name + '</span><span class ="listHyp">' +comments[i].date +'</span></p><p>'+ comments[i].comment + '</p></li>';
+                                list=list+'<li class="hiddenlist"> </li>';
+                            }
+                  
+                            var nameList = document.getElementById("commentList");
+                            nameList.innerHTML = list;
+                        }
+                }
+                
+                };
+               
+                var pageH = document.getElementById("myHeader");
+
+     
+                var page='';
+                page=pageH.innerHTML; 
+                alert("The inner html = "+page);
+               
+                requestList.open('GET', 'http://sandeep69.imad.hasura-app.io/'+page+'/get_comment_list',true);
+                requestList.send(null);
+ 
+ 
+ 
+                //ends here
+            }
+        }
+       
+    };
+    
+  
+    request.open('GET', 'http://sandeep69.imad.hasura-app.io/check-login',true);
+    request.send(null);
+  
 });
 
 
