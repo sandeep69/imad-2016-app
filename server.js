43c22251bc4ef4282599dcc60e0044e3548db4ad
sandeep69 <s_j_abraham@hotmail.com>
2016-11-21 10:16:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -238,7 +238,6 @@ app.post ('/login', function(req,res){
 app.get('/check-login', function(req,res){
     if (req.session && req.session.auth && req.session.auth.userId){
         //return the username
-/*        
          pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId],function(err,result){
                 if(err) {
                     res.status(500).send(err.toString());
@@ -250,8 +249,9 @@ app.get('/check-login', function(req,res){
                      res.send(name);
                 }
             });
-*/
+/*            
         res.send("You are logged in as :" + req.session.auth.userId.toString());
+*/
     } else {
         res.send("You are not logged in");
     }
