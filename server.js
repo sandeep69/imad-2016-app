b4e278cb922ad6d8bcce30164f99028af431fd35
sandeep69 <s_j_abraham@hotmail.com>
2016-11-22 10:31:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -308,7 +308,7 @@ app.get('/:an/likes', function (req, res) {
           }
           else {
              var likes = result.rows[0].likes;
-             if (inc === 1){
+             if (inc === '1'){
                  ++likes;
                  pool.query("INSERT INTO article (likes) VALUES ($1) WHERE title = $2", [likes,an],function(err,result){
                     if (err) {
