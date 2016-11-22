84d269fcf7455d34a5c9535d1aab7a9bde913771
sandeep69 <s_j_abraham@hotmail.com>
2016-11-22 14:36:01 +0530

    [imad-console] Updates ui/mainPage.js

--- ui/mainPage.js
+++ ui/mainPage.js
@@ -1,3 +1,33 @@
+function check_tabs() {//check if user logged in- put signup/signOut
+   
+    //check if user has logged in  
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+         
+        //check it result has been loaded
+        if(request.readyState === XMLHttpRequest.DONE){
+            //if success - user logged in - username returned - display comment box
+            if (request.status === 200) {
+                //already signed in change the signup/register 
+	           var signUp = document.getElementById("signUpTab");
+	           signUp.style.display = "none";
+	           var signOut = document.getElementById("signOutTab");
+	           signOut.style.display = "block";
+	       }
+	           
+        }
+       
+    };
+    
+    //check if user is logged in - it returns username  
+    request.open('GET', 'http://sandeep69.imad.hasura-app.io/check-login',true);
+    request.send(null);
+
+}
+
+
+
 //to load available list of articles from Data base
 // load all the articles names
 
@@ -27,6 +57,7 @@ $(document).ready(function(){
                 console.log( nameList.innerHTML); 
               
             }
+            check_tabs(); 
         }
         
     };
