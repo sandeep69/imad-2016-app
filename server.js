3d2911e1d67873bd89c32fc6eac1653f0e04d79b
sandeep69 <s_j_abraham@hotmail.com>
2016-11-22 08:58:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -298,9 +298,8 @@ app.get('/listOfArticles',function(req,res){
 
 app.get('/:an/likes', function (req, res) {
     var an = req.params.an;
-    res.send(an);
-   /*
-    pool.query("SELECT * FROM article WHERE title=$1", [an], function(err,result){
+  
+    pool.query("SELECT * FROM article WHERE title = $1", [an], function(err,result){
       if (err) {
           res.status(500).send(err.toString());
       } else {
@@ -313,7 +312,7 @@ app.get('/:an/likes', function (req, res) {
           }
       }
     });
-    */
+   
 });
 
 
