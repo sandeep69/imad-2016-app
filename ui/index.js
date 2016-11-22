5b0f3bfb403c98ad349dab5d8775f0c9b0ad065d
sandeep69 <s_j_abraham@hotmail.com>
2016-11-22 14:25:59 +0530

    [imad-console] Updates ui/index.js

--- ui/index.js
+++ ui/index.js
@@ -1,1 +1,30 @@
 //when index starts up
+$(document).ready(function() { //remember to include jquery.min.js in the html file
+   
+    //check if user has logged in  
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+         
+        //check it result has been loaded
+        if(request.readyState === XMLHttpRequest.DONE){
+            //if success - user logged in - username returned - display comment box
+            if (request.status === 200) {
+                //already signed in change the signup/register 
+	           var signUp = document.getElementById("signUpTab");
+	           signUp.style.display = "none";
+	           var signOut = document.getElementById("signOutTab");
+	           signOut.style.display = "block";
+	                
+	                
+	        }
+	           
+        }
+       
+    };
+    
+    //check if user is logged in - it returns username  
+    request.open('GET', 'http://sandeep69.imad.hasura-app.io/check-login',true);
+    request.send(null);
+
+});
